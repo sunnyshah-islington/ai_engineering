a98129e4d8f37c2bb0f30075879c55cddb8c671b
Sunny Shah <sunnnny619@gmail.com>
2026-07-19 12:54:48 +0545

    created gitignore, added requirements.in and another model

--- apis/main.py
+++ apis/main.py
@@ -1,62 +1,67 @@
-from pydantic import BaseModel
-
 from fastapi import FastAPI
 from transformers import pipeline
 
-from models_validation import ModelStatus, ReviewRequest, GeneratedResponse, SentimentResponse
+from apis.models_validation import (
+    ModelStatus,
+    ReviewRequest,
+    GeneratedResponse,
+    SentimentResponse,
+)
 
 
 def load_model():
     return pipeline(
         task="text-generation",
-		model="Qwen/Qwen2.5-1.5B"
+        model="Qwen/Qwen2.5-1.5B",
     )
 
 
 def load_ai_sentiment_model():
-	return pipeline(
-		task="sentiment-analysis",
+    return pipeline(
+        task="sentiment-analysis",
         model="distilbert-base-uncased-finetuned-sst-2-english",
-	)
+    )
 
 
 ml = {}
+
+
 async def lifespan(app: FastAPI):
-	ml['text_generator'] = load_model()
-	ml['sentiment_analyzer'] = load_ai_sentiment_model()
-	yield
-	ml.clear()
+    ml["text_generator"] = load_model()
+    ml["sentiment_analyzer"] = load_ai_sentiment_model()
+    yield
+    ml.clear()
 
 
 app = FastAPI(
-	title="AI Engineering with FastAPI and Transformers(HF models)",
-	description="A simple FastAPI application that uses Hugging Face Transformers for text generation and sentiment analysis.",
-	lifespan=lifespan
+    title="AI Engineering with FastAPI and Transformers(HF models)",
+    description="A simple FastAPI application that uses Hugging Face Transformers for text generation and sentiment analysis.",
+    lifespan=lifespan,
 )
 
 
 @app.get("/", tags=["health"])
 def health_checks():
-	return ModelStatus(
-		model_loaded="text_generator" in ml and "sentiment_analyzer" in ml,
-		status="Server Healthy and Running" if "text_generator" in ml and "sentiment_analyzer" in ml else "Server Unhealthy: Models not loaded"
-	)
+    return ModelStatus(
+        model_loaded="text_generator" in ml and "sentiment_analyzer" in ml,
+        status=("Server Healthy and Running" if "text_generator" in ml and "sentiment_analyzer" in ml else "Server Unhealthy: Models not loaded"),
+    )
 
 
 @app.post("/generate_text", response_model=GeneratedResponse, tags=["text-generation"])
 async def generate(request: ReviewRequest):
-	result = ml['text_generator'](request.prompt)[0]
-	return GeneratedResponse(
-		prompt_text=request.prompt,
-		generated_text=result['generated_text']
-	)
+    result = ml['text_generator'](request.prompt)[0]
+    return GeneratedResponse(
+        prompt_text=request.prompt,
+        generated_text=result["generated_text"]
+    )
 
 
 @app.post("/analyze_sentiment", response_model=SentimentResponse, tags=["sentiment-analysis"])
 async def analyze_sentiment(request: ReviewRequest):
-	result = ml['sentiment_analyzer'](request.prompt)[0]
-	return SentimentResponse(
-		prompt_text=request.prompt,
-		sentiment=result['label'],
-		confidence=round(float(result['score']), 4)
-	)
+    result = ml["sentiment_analyzer"](request.prompt)[0]
+    return SentimentResponse(
+        prompt_text=request.prompt,
+        sentiment=result["label"],
+        confidence=round(float(result["score"]), 4),
+    )
